650bbda77d2088b956ee70956289ae5afa955299
tanviigg <tanvi9g@gmail.com>
2025-11-18 17:16:23 -0800

    Deploy React portfolio to main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <nav style={{ padding: "20px", textAlign: "center", borderBottom: "1px solid #ccc" }}>
+    <nav style={{ padding: "10px", textAlign: "center", borderBottom: "1px solid #8d8989ff" }}>
       <Link to="/" style={{ margin: "0 15px" }}>Home</Link>
       <Link to="/about" style={{ margin: "0 15px" }}>About</Link>
       <Link to="/projects" style={{ margin: "0 15px" }}>Projects</Link>
