082b88e19ed0b315fa7eff3ee094fb05b0887075
tanviigg <tanvi9g@gmail.com>
2025-11-18 17:43:01 -0800

    Fixed landing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,21 @@ function Hero() {
   return (
     <section style={{ textAlign: "center", padding: "100px 20px" }}>
       <h1>Hi, I'm Tanvi Gupta</h1>
-      <p>Welcome to my portfolio!</p>
+  <p>
+      I am currently an undergraduate student studying Computer Science with a passion for building systems that combine creativity and technology.
+    </p>
+
+    <p>
+      I am currently working at <strong>ASPR AI</strong> and coauthoring a technical paper detailing the extensive process of LLM benchmarking to guide future model selection and strategy in sales.
+    </p>
+
+    <p>
+      In my free time, I am building a <strong>Gesture-Based Music Controller</strong> that uses hand movements to control sound in real time.
+    </p>
+
+    <p>
+      I’m always looking to collaborate on innovative projects. Let's build something cool together!
+    </p>
     </section>
   );
 }
